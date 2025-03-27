d200c6792383f6828c2d551e70fc863e7096f36b
Álvaro Pereda Sánchez <121577598+AlvaroPereda@users.noreply.github.com>
2025-03-27 17:58:05 +0000

    prueba2

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,6 @@ import IndexMenu from "../components/IndexMenu.tsx";
 
 export default function Home() {
   const aux = Deno.env.get("PRUEBA")
-  console.log(aux)
+  console.log("Prueba: " + aux)
   return <IndexMenu/>
 }
